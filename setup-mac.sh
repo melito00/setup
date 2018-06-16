c93320e3435bdefb5ce840b4b8f3d3c9ea2d6ac3
Kenji Yamada <kenji.yamada@gmail.com>
2018-06-16 14:30:58 +0100

    add setup-mac.sh

---
+++ setup-mac.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# sudo xcodebuild -license
+
+echo "Install Homebrew"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# brew update
+# brew install ansible
+# git clone https://github.com/melito00/setup
+# cd setup
+# brew bundle
+# mkdir $HOME/work
+# cd $HOME/work
+# git clone https://github.com/melito00/ansible_repo
+# git remote set-url origin git@github.com:melito00/setup.git
+# cd $HOME/work/ansible_repo
+# git remote set-url origin git@github.com:melito00/ansible_repo.git
